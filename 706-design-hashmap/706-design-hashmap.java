adb4b05ccae100fc2ee5fd8de1c8e7f7d054d453
Yash Raj Ojha <67517768+yashrajOjha@users.noreply.github.com>
2022-03-02 23:23:13 +0530

    Time: 12 ms (99.00%), Space: 48.3 MB (84.84%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.java
+++ 706-design-hashmap/706-design-hashmap.java
@@ -1,27 +1,81 @@
 class MyHashMap {
-    int map[];
+
+    Node[] map = new Node[227];
     public MyHashMap() {
-        map = new int[1000001];
-        Arrays.fill(map,-1);
+        for(int i=0;i<map.length;i++){
+            map[i] = null;
+        }
     }
     
     public void put(int key, int value) {
-        map[key]=value;
+        int hashCode  =  getHash(key);
+        Node currNode= map[hashCode];
+        Node prevNode = null;
+        if(currNode==null){
+            map[hashCode] = new Node();
+            map[hashCode].key = key;
+            map[hashCode].value = value;
+            return;
+        }        
+        while(currNode!=null){            
+            if(currNode.key==key){
+                currNode.value = value;
+                return;
+            }
+            prevNode = currNode;
+            currNode = currNode.next;
+        }
+        prevNode.next = new Node();
+        prevNode.next.key = key;
+        prevNode.next.value = value;
+        return;
     }
-    
+
     public int get(int key) {
-        return map[key];
+        int hashCode = getHash(key);
+        Node currNode= map[hashCode];
+        Node prevNode = null;     
+
+        while(currNode!=null){
+            if(currNode.key==key){
+                return currNode.value;
+            } 
+            prevNode = currNode;
+            currNode = currNode.next;
+        }
+        return -1;
     }
     
     public void remove(int key) {
-        map[key]=-1;
+        int hashCode =  getHash(key);
+        
+        Node currNode = map[hashCode];
+        Node prevNode = null;
+        Node nextNode = null;
+        if(currNode!=null&&currNode.key==key){
+                map[hashCode] = map[hashCode].next;
+                return;
+        }
+        
+        while(currNode!=null){
+            nextNode = currNode.next;            
+            if(currNode.key==key&&prevNode!=null){
+                prevNode.next=nextNode;
+                return;
+            }
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+    }
+    
+    public int getHash(int key){
+        return key % 227;
     }
 }
 
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap obj = new MyHashMap();
- * obj.put(key,value);
- * int param_2 = obj.get(key);
- * obj.remove(key);
- */
+class Node{
+    Node(){};
+    public int key=-1;
+    public int value=-1;
+    public Node next=null;
+}
