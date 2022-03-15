5d61918a6f612e46015caac6fe6562430e201f02
Yash Raj Ojha <67517768+yashrajOjha@users.noreply.github.com>
2022-03-15 23:53:40 +0530

    Time: 8 ms (99.45%), Space: 42.7 MB (91.35%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+     int count = 0;
+        
+        char ch[] = s.toCharArray();
+        
+        for(int i = 0; i < ch.length; i++){
+            if(ch[i] == '(') count++;
+            else if(ch[i] == ')'){
+                if(count == 0) ch[i] = '#';
+                else count--;
+            }
+        }
+        
+        for(int i = ch.length - 1; i >= 0; i--){
+            if(ch[i] == '(' && count > 0){
+                ch[i] = '#';
+                count--;
+            }
+            
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < ch.length; i++){
+            if(ch[i] != '#') sb.append(ch[i]);
+        }
+        return sb.toString();   
+    }
+}
