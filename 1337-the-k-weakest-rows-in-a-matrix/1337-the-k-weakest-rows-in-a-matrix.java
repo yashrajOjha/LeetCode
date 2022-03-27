32cbb361b365d87f3d85d51c93fcbe42d3fc3490
Yash Raj Ojha <67517768+yashrajOjha@users.noreply.github.com>
2022-03-27 23:03:03 +0530

    Time: 2 ms (88.13%), Space: 48.8 MB (49.95%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,24 +1,38 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-        int count[] = new int[mat.length];
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[0].length;j++){
-                if(mat[i][j]==1) count[i]+=1;
-            }
-        }
-        int index[]= new int[k];
-        for(int i=0;i<k;i++){
-            int min = Integer.MAX_VALUE;
-            int minIndex=0;
-            for(int j=0;j<count.length;j++){
-                if(count[j]<min){
-                    min = count[j];
-                    minIndex=j;
+        //declaring a comparator
+        Comparator <int[]> customCompare = new Comparator<>(){
+            @Override
+            public int compare(int a[],int b[]){
+                if(a[0]==b[0]){
+                    return b[1]-a[1];
+                }else{
+                    return b[0]-a[0];
                 }
             }
-            index[i]=minIndex;
-            count[minIndex] = Integer.MAX_VALUE;
+        };
+        PriorityQueue<int []> pq = new PriorityQueue<>(customCompare); //count and row index 
+        for(int i=0;i<mat.length;i++){
+            int []row = mat[i];
+            int countOnes = getCount(row);
+            pq.offer(new int[]{countOnes,i});
+            if(pq.size()>k){
+                pq.poll();
+            }
+            }
+        int ans[] = new int[k];
+            int i=k-1;
+            while(!pq.isEmpty()){
+                ans[i]=pq.poll()[1];
+                i--;
+            }
+        return ans;
+    }
+    public int getCount(int [] rows){
+        int count=0;
+        for(int ele:rows){
+            if(ele==1) count++;
         }
-        return index;
+        return count;
     }
 }
