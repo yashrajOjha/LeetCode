e4a6a6744847cac7e806ce15a6ca7db0ba157528
Yash Raj Ojha <67517768+yashrajOjha@users.noreply.github.com>
2022-04-22 00:14:56 +0530

    Time: 32 ms (47.11%), Space: 82.6 MB (17.18%) - LeetHub

--- 705-design-hashset/705-design-hashset.java
+++ 705-design-hashset/705-design-hashset.java
@@ -1,23 +1,20 @@
 class MyHashSet {
-    int[] set;
+    boolean[] set;
     public MyHashSet() {
-        set = new int[1000001];
-        Arrays.fill(set,-1);
+        set = new boolean[1000001];
+        // Arrays.fill(set,-1);
     }
     
     public void add(int key) {
-        set[key] = key;
+        set[key] = true;
     }
     
     public void remove(int key) {
-        set[key]=-1;
+        set[key]=false;
     }
     
     public boolean contains(int key) {
-        if(set[key]!=-1) return true;
-        else{
-            return false;
-        }
+        return set[key];
     }
 }
 
