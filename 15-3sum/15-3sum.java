a9bcf44c3fde0053fd14555b2c048213eefe344f
Yash Raj Ojha <67517768+yashrajOjha@users.noreply.github.com>
2022-07-04 20:36:55 +0530

    Time: 36 ms (50.05%), Space: 59.1 MB (48.35%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,23 +1,22 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] num) {
-    Arrays.sort(num);
-    List<List<Integer>> res = new LinkedList<>(); 
-    for (int i = 0; i < num.length-2; i++) {
-        //until third last element so we can form a triplet
-        if (i == 0 || (i > 0 && num[i] != num[i-1])) {
-            int lo = i+1, hi = num.length-1, sum = 0 - num[i];
-            while (lo < hi) {
-                if (num[lo] + num[hi] == sum) {
-                    res.add(Arrays.asList(num[i], num[lo], num[hi]));
-                    //ignoring same next values of num[lo] and num[high]
-                    while (lo < hi && num[lo] == num[lo+1]) lo++;
-                    while (lo < hi && num[hi] == num[hi-1]) hi--;
-                    lo++; hi--;
-                } else if (num[lo] + num[hi] < sum) lo++;
-                else hi--;
-           }
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        int n=nums.length;
+        for(int i=0;i<n-2;i++){
+                if(i==0 || (i>0 && nums[i]!=nums[i-1])){
+                    int l=i+1,h=n-1,sum=0-nums[i];
+                    while(l<h){
+                        if(nums[l]+nums[h]==sum){
+                           res.add(Arrays.asList(nums[i], nums[l], nums[h]));
+                           while(l<h && nums[l]==nums[l+1])l++;
+                           while(l<h && nums[h]==nums[h-1])h--;
+                           l++;h--;
+                        }else if(nums[l]+nums[h]>sum) h--;
+                        else l++;
+                    }
+                }           
         }
-    }
-    return res;
+        return res;
     }
 }
