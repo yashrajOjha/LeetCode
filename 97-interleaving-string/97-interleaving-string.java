50ead2b5782f07164d70341ad1ed3a3eadce17dd
Yash Raj Ojha <67517768+yashrajOjha@users.noreply.github.com>
2022-07-07 12:34:36 +0530

    Time: 9 ms (30.18%), Space: 42.3 MB (57.44%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,32 +1,32 @@
 class Solution {
-    public boolean check(String s1, String s2, String s3, int i, int j, Boolean [][]mem){
-        //base case;
-        if(i==s1.length() && j==s2.length()){
-            return mem[i][j]=true;
-        }
-        if(mem[i][j] != null){
-            return mem[i][j];
-        }
-        if(i<s1.length() && s1.charAt(i)==s3.charAt(i+j)){
-            mem[i][j] = check(s1, s2, s3, i+1,j,mem);
-            if(mem[i][j]==true){
-                return true;
-            }
-        }
-        if(j<s2.length() && s2.charAt(j)==s3.charAt(i+j)){
-            mem[i][j] = check(s1, s2, s3, i,j+1,mem);
-            if(mem[i][j]==true){
-                return true;
-            }
-        }
-        mem[i][j]=false;
-        return mem[i][j];
-    }
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1.length()+s2.length()!=s3.length()){
+        int n1=s1.length();
+        int n2=s2.length();
+        if(n1+n2!=s3.length())
             return false;
+        boolean dp[][]=new boolean[n1+1][n2+1];
+        dp[0][0]=true;
+        for(int j=1;j<=n2;j++)
+        {//initializing 0th row
+            if(s2.charAt(j-1)==s3.charAt(j-1))
+                dp[0][j]=dp[0][j-1];
         }
-        Boolean mem[][] = new Boolean[s1.length()+1][s2.length()+1];
-        return check(s1,s2,s3,0,0,mem); 
+        for(int i=1;i<=n1;i++)
+        {//initializing 0th column
+            if(s1.charAt(i-1)==s3.charAt(i-1))
+                dp[i][0]=dp[i-1][0];
+        }
+        for(int i=1;i<=n1;i++)
+        {
+            for(int j=1;j<=n2;j++)
+            {//the length of s3 is n1+n2, since the string is 0 indexed we check at i+j-1
+                if(s3.charAt(i+j-1)==s1.charAt(i-1))
+                    dp[i][j]=dp[i-1][j];
+                if(s3.charAt(i+j-1)==s2.charAt(j-1))
+                    dp[i][j]=dp[i][j]||dp[i][j-1];  
+					//In some cases the character from s3 matches both s1 and s2, therefore we consider the value thats true
+            }
+        }
+        return dp[n1][n2];
     }
 }
