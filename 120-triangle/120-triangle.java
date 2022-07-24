d85828671da034c146a3fb59851af731d6088b7a
Yash Raj Ojha <67517768+yashrajOjha@users.noreply.github.com>
2022-07-24 23:56:18 +0530

    Time: 4 ms (64.77%), Space: 44.6 MB (20.39%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,18 +1,19 @@
 class Solution {
-    public int func(List<List<Integer>> triangle, int row, int col, int dp[][]){
-        if(row==triangle.size()-1) return triangle.get(row).get(col);
-        
-        if(dp[row][col]!=-1) return dp[row][col];
-        
-        int down = triangle.get(row).get(col) + func(triangle,row+1, col, dp);
-        int diag = triangle.get(row).get(col) + func(triangle, row+1, col+1, dp);
-        return dp[row][col] = Math.min(down,diag);
-    }
     public int minimumTotal(List<List<Integer>> triangle) {
-        int dp[][] = new int[triangle.size()][triangle.size()];
-        for(int i=0;i<triangle.size();i++){
-            Arrays.fill(dp[i],-1);
+        int n = triangle.size();
+        int dp[][] = new int[n][n];
+        //base case, can be anything from the bottom row
+        for(int j=0;j<n;j++){
+            dp[n-1][j] = triangle.get(n-1).get(j);
         }
-        return func(triangle,0,0,dp);
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down = triangle.get(i).get(j) + dp[i+1][j];
+                int diag = triangle.get(i).get(j) + dp[i+1][j+1];
+                dp[i][j] = Math.min(down, diag);
+            }
+        }
+        //because we started from 0,0 and not m-1,n-1 the answer is in 0,0
+        return dp[0][0];
     }
 }
