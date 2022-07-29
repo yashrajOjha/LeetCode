e9f1f5073d71a05afb280e209abdabd34b75f178
Yash Raj Ojha <67517768+yashrajOjha@users.noreply.github.com>
2022-07-29 23:43:23 +0530

    Time: 110 ms (12.64%), Space: 98.5 MB (27.12%) - LeetHub

---
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canArrange(int[] arr, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : arr) {
+            int rem = n % k;
+            if (rem < 0) rem += k;
+            map.put(rem, map.getOrDefault(rem, 0) + 1);
+        }
+        for (int key : map.keySet()) {
+            if (key == k - key || key == 0) {if (map.get(key) % 2 != 0) return false;}
+            else if (!map.get(key).equals(map.getOrDefault(k - key, 0))) return false;  // can not use == here, Integer boxed;
+        }
+        return true;
+    }
+}
