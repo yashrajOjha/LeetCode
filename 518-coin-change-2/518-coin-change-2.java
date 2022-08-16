4ac2bb034a2c174aeae1f32fd1c3cd2a17a1a693
Yash Raj Ojha <67517768+yashrajOjha@users.noreply.github.com>
2022-08-16 21:56:14 +0530

    Time: 15 ms (43.15%), Space: 51.1 MB (34.88%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -1,23 +1,18 @@
 class Solution {
-    public int func(int index, int target, int coins[], int dp[][]){
-        if(index==0){
-            if(target%coins[0]==0) return 1;
-            else return 0;
-        }
-        
-        if(dp[index][target]!=-1) return dp[index][target];
-        
-        int nT = func(index-1,target,coins,dp);
-        int T = 0;
-        if(coins[index]<=target) T = func(index,target-coins[index],coins,dp);
-        
-        return dp[index][target]=nT+T;
-    }
     public int change(int amount, int[] coins) {
         int n = coins.length;
         int dp[][] = new int[n][amount+1];
-        for(int row[]:dp) Arrays.fill(row,-1);
+        for(int tar=0;tar<=amount;tar++) dp[0][tar] = (tar%coins[0]==0)?1:0;
         
-        return func(n-1,amount,coins,dp);
+        for(int i=1;i<n;i++){
+            for(int tar=0;tar<=amount;tar++){
+                int nT = dp[i-1][tar];
+                int T = 0;
+                if(coins[i]<=tar) T = dp[i][tar-coins[i]];
+                
+                dp[i][tar]=T+nT;
+            }
+        }
+        return dp[n-1][amount];
     }
 }
