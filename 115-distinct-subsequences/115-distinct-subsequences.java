e5b0419b6ca4913e017c53f73716d938c7556240
Yash Raj Ojha <67517768+yashrajOjha@users.noreply.github.com>
2022-08-30 00:24:22 +0530

    Time: 28 ms (57.01%), Space: 50.6 MB (22.08%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -1,34 +1,41 @@
 class Solution {
-    public int func(int ind1, int ind2, String s, String t,int dp[][]){
-        if(ind2<0) return 1;
-        if(ind1<0) return 0;
+//     public int func(int ind1, int ind2, String s, String t,int dp[][]){
+//         if(ind2<0) return 1;
+//         if(ind1<0) return 0;
         
-        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+//         if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
         
-        if(s.charAt(ind1)==t.charAt(ind2)){
-            return dp[ind1][ind2]=func(ind1-1,ind2-1,s,t,dp) + func(ind1-1,ind2,s,t,dp);
-        }else{
-            return dp[ind1][ind2]=func(ind1-1,ind2,s,t,dp);
-        }
-    }
+//         if(s.charAt(ind1)==t.charAt(ind2)){
+//             return dp[ind1][ind2]=func(ind1-1,ind2-1,s,t,dp) + func(ind1-1,ind2,s,t,dp);
+//         }else{
+//             return dp[ind1][ind2]=func(ind1-1,ind2,s,t,dp);
+//         }
+//     }
     public int numDistinct(String s, String t) {
-        int n=s.length(),m=t.length();
-        int dp[][] = new int[n+1][m+1];
-        // for(int rows[]:dp) Arrays.fill(rows,-1);
-        // return func(s.length()-1,t.length()-1,s,t,dp);
-        for(int i=0;i<=n;i++)  dp[i][0]=1;
-        for(int j=1;j<=m;j++)  dp[0][j]=0;
+        int l1=s.length(),l2=t.length();
+        int[] prev=new int[l2+1];
+        int[] curr=new int[l2+1];
         
-        //tabulation is reverse of 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s.charAt(i-1)==t.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                }else{
-                    dp[i][j] = dp[i-1][j];
+        prev[0]=1;  // Initializaton 
+        curr[0]=1;
+        
+        for(int ptr1=1;ptr1<=l1;ptr1++)
+        {
+            for(int ptr2=1;ptr2<=l2;ptr2++)
+            {
+
+                if(s.charAt(ptr1-1)==t.charAt(ptr2-1))
+                {
+                    int take=prev[ptr2-1];
+                    int notTake=prev[ptr2];
+                    curr[ptr2]=take+notTake;
+                }
+                else{
+                    curr[ptr2]=prev[ptr2];
                 }
             }
+            prev=(int[])curr.clone();
         }
-        return dp[n][m];
+        return curr[l2];
     }
 }
