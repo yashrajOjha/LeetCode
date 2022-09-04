7c0e6447448424add5ce223726a34418907d433e
Yash Raj Ojha <67517768+yashrajOjha@users.noreply.github.com>
2022-09-04 10:51:05 +0530

    Time: 0 ms (100.00%), Space: 41 MB (33.45%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -1,13 +1,17 @@
 class Solution {
-    public int func(int n){
+    public int func(int n, int dp[]){
         if(n<=1) return 1;
+        if(dp[n]!=-1) return dp[n];
         int ans= 0;
         for(int i=1;i<=n;i++){
-            ans+=func(i-1)*func(n-i);
+            ans+=func(i-1,dp)*func(n-i,dp);
+            dp[n]=ans;
         }
-        return ans;
+        return dp[n];
     }
     public int numTrees(int n) {
-        return func(n);
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+        return func(n,dp);
     }
 }
