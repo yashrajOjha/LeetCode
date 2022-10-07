ade0d5c79d186ae0441dea51f11a9c1652d4e96a
Yash Raj Ojha <67517768+yashrajOjha@users.noreply.github.com>
2022-10-07 10:38:41 +0530

    Time: 0 ms (100.00%), Space: 39 MB (95.24%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,14 +1,18 @@
 class Solution {
-    public int func(int i, int n){
+    public int func(int i, int n,int dp[][]){
         if(n==0) return 1;
-        if(i<=0) return 0; //considering a vowel string has just 5 letters
+        if(i<0) return 0; //considering a vowel string has just 5 letters
         
-        int pick = func(i,n-1);
-        int notpick = func(i-1,n);
+        if(dp[i][n]!=-1) return dp[i][n];
         
-        return pick+notpick;
+        int pick = func(i,n-1,dp);
+        int notpick = func(i-1,n,dp);
+        
+        return dp[i][n]=pick+notpick;
     }
     public int countVowelStrings(int n) {
-        return func(5,n);
+        int dp[][] = new int[5][n+1];
+        for(int rows[]:dp) Arrays.fill(rows,-1);
+        return func(4,n,dp);
     }
 }
